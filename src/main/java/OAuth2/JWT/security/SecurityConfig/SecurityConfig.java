16b7f07c2c8b1c020431458324c1be6c6159d191
Azamat Orozbaev <44497623+songhee24@users.noreply.github.com>
2020-12-30 01:50:38 +0600

    created security with oauth2 login google

---
+++ src/main/java/OAuth2/JWT/security/SecurityConfig/SecurityConfig.java
@@ -0,0 +1,20 @@
+package OAuth2.JWT.security.SecurityConfig;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    public void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf().disable().antMatcher("/**").authorizeRequests()
+                .antMatchers("/home").permitAll()
+                .antMatchers("/", "/index.html").authenticated()
+                .anyRequest().authenticated()
+                .and()
+                .oauth2Login().permitAll()
+                .and()
+                .logout().logoutSuccessUrl("/");
+    }
+}
