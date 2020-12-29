4a963598b7476298c480c856f2198de0be09fe1d
Azamat Orozbaev <44497623+songhee24@users.noreply.github.com>
2020-12-30 01:18:31 +0600

    create OAuth 2 app

---
+++ src/main/java/OAuth2/JWT/security/SecurityApplication.java
@@ -0,0 +1,14 @@
+package OAuth2.JWT.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityApplication.class, args);
+	}
+
+
+}
